9b938e185b954d14fb96e474397693960c892fba
Ankith Jain <ankithjain@Ankiths-MacBook-Pro.local>
2020-04-03 22:38:16 -0500

    cors origin multiple sources

--- src/main/java/com/corona/covid19/configuration/CorsConfiguration.java
+++ src/main/java/com/corona/covid19/configuration/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200","https://covid-19-app-pn.herokuapp.com");
             }
         };
     }
